9319cfdf0b749166a1558343f9c5821645cd7d24
vanevery <vanevery@walking-productions.com>
2014-12-13 22:07:46 -0500

    Removed some logging

--- src/info/guardianproject/securereader/SyncService.java
+++ src/info/guardianproject/securereader/SyncService.java
@@ -107,9 +107,6 @@ public class SyncService extends Service {
     	
     	//SyncTask(Feed _feed, SyncServiceFeedFetcher.SyncServiceFeedFetchedCallback _callback) {
     	SyncTask(Feed _feed) {
-    		if (LOGGING) 
-    			Log.v(LOGTAG,"SyncTask: " + _feed.getFeedURL());
-    		
     		feed = _feed;
     		type = TYPE_FEED;
     		//callback = _callback;
