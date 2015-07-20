591138d5ff47b9821c9413640f12639fffe285f1
vanevery <vanevery@walking-productions.com>
2015-07-20 13:13:30 -0400

    Database should be at version 2

--- src/info/guardianproject/securereader/DatabaseHelper.java
+++ src/info/guardianproject/securereader/DatabaseHelper.java
@@ -12,7 +12,7 @@ public class DatabaseHelper extends SQLCipherOpenHelper
 	public static boolean LOGGING = false;
 
 	public static final String DATABASE_NAME = "bigbuffalo.db";
-	public static final int DATABASE_VERSION = 1;
+	public static final int DATABASE_VERSION = 2;
 
 	public static final int POSTS_FEED_ID = -99;
 	public static final int DRAFTS_FEED_ID = -98;
